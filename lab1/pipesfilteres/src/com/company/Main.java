4d5a64e8a8fdf0c52cb9e305c7c5ecc723b940ac
gitdots <bogdanbaciu00@yahoo.com>
2022-04-02 11:36:32 +0300

    first part

---
+++ lab1/pipesfilteres/src/com/company/Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        FurnitureFactory factory = new FurnitureFactory("Factory");
+        ChairProcess cp = ChairProcess.getInstance();
+
+        factory.hireWorker("Ionel", cp.process("CutSeat").getActivity());
+        factory.hireWorker("Armin", cp.process("CutSeat").getActivity());
+        factory.hireWorker("Erwin", cp.process("Package").getActivity());
+        factory.hireWorker("Dans", cp.process("AssembleBackrest").getActivity());
+        factory.hireWorker("Sai", cp.process("AssembleStabilizer").getActivity());
+        factory.hireWorker("Alb", cp.process("AssembleStabilizer").getActivity());
+
+        ArrayList<Pair<String, Integer>> firstLineProcess = new ArrayList<>();
+        firstLineProcess.add(cp.process("CutSeat"));
+        firstLineProcess.add(cp.process("AssembleBackrest"));
+        firstLineProcess.add(cp.process("AssembleStabilizer"));
+        firstLineProcess.add(cp.process("Package"));
+        System.out.println(firstLineProcess);
+
+        factory.openWorkingLine("First Line",
+                new WorkingLine("FirstLine", firstLineProcess));
+
+        factory.startLine("First Line");
+        System.out.println(factory);
+    }
+}
